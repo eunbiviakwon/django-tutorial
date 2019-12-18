f38af0359c0c6cd7dd207970f17b21766d713096
eunbiviakwon <eunbiviakwon@gmail.com>
2019-12-18 13:26:54 +0900

    url namespace 사용

--- polls/views.py
+++ polls/views.py
@@ -4,14 +4,16 @@ from django.template import loader
 
 from .models import Question
 
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
-        'latest_question_list' : latest_question_list,
-
+        'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+    # template = loader.get_template('polls/index.html')
+    # return HttpResponse(template.render(context, request))
+
 
 def detail(request, question_id):
     try:
@@ -24,9 +26,11 @@ def detail(request, question_id):
     }
     return render(request, 'polls/detail.html', context)
 
+
 def results(request, question_id):
     response = "You're looking at the results of question {}"
     return HttpResponse(response.format(question_id))
+
 
 def vote(request, question_id):
     return HttpResponse(f"You're voting on question {question_id}.")
